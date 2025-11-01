cb8a7bb87aa201dc2ad15c8218a7e4b7c3919ff4
jtestrosos <jamesestrosos0729@gmail.com>
2025-11-01 22:24:31 +0800

    test

--- resources/views/admin/inventory.blade.php
+++ resources/views/admin/inventory.blade.php
@@ -176,9 +176,7 @@
 
                     <!-- Content -->
                                             @if($inventory->count() > 0)
-
                     <div class="p-4">
-
                     <div class="d-flex justify-content-end mb-4">
                         <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addItemModal">
                                 <i class="fas fa-plus me-2"></i> Add Inventory
