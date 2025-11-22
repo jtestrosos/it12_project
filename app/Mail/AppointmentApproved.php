bfb0be0d359fe027ad3f36f68f327b9351143055
LJPATHAY <alta60785@gmail.com>
2025-11-22 17:50:05 +0800

    optimized the loading for approving appointment

--- app/Mail/AppointmentApproved.php
+++ app/Mail/AppointmentApproved.php
@@ -4,10 +4,11 @@ namespace App\Mail;
 
 use App\Models\Appointment;
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class AppointmentApproved extends Mailable
+class AppointmentApproved extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
